dd9e58685a88c0a574d09962093af982f05d123d
Dalia Elbanhawy <daliaelbanhawy@google.com>
2021-04-22 11:28:55 +0100

    done

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,40 @@ const router = express.Router();
 const User = require("../models/User.model")
 const bcrypt = require("bcryptjs");
 
+router.get("/login", (req,res)=> {
+    res.render("login");
+  }); 
+
+
+  router.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    if (username === "" || password === "") {
+      res.render("login", 
+      {errorMessage: "Indicate username and password"});
+      return;
+    }
+  
+    const user = await User.findOne({username})
+    if (user === null) {
+      res.render("login", 
+      {errorMessage: "Invalid login"}); 
+      return;
+    }
+
+    if (bcrypt.compareSync(password, user.password)){
+        //succesful login
+        req.session.currentUser = user;
+        //res.redirect("/");
+        res.render("index", {user});
+    
+      } else { //password doesn't match
+        res.render("login", 
+        {errorMessage: "Invalid login"}); 
+        return;
+      }
+    });
+      
+
 
 router.get("/signup", (req,res) => {
     res.render("signup");
@@ -50,6 +84,12 @@ router.get("/signup", (req,res) => {
       
     }
   });
+
+  router.post("/logout", (req,res)=> {
+    req.session.destroy();
+    res.redirect("/");
+  });
+  
   
 
 module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+
+function requireLogin(req,res,next) {
+    if (req.session.currentUser) {
+      next();
+    }  else {
+        res.redirect("/login")
+      }
+  }
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+router.get("/private", requireLogin, (req,res) => {
+    res.render("private");
+  });
+  
 module.exports = router;
